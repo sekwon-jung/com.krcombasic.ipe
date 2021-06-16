61d08cb52d37f19f5fd3f26047552c7684198c84
sekwon <bus3book@gmail.com>
2021-06-16 16:20:51 +0900

    HelloController Test 통과

--- src/main/java/com/krcombasic/ipe/springboot/web/HelloController.java
+++ src/main/java/com/krcombasic/ipe/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.krcombasic.ipe.springboot.web;
 
+import com.krcombasic.ipe.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -11,4 +13,10 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
 }
--- src/main/java/com/krcombasic/ipe/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/krcombasic/ipe/springboot/web/dto/HelloResponseDto.java
@@ -2,12 +2,13 @@ package com.krcombasic.ipe.springboot.web.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
-//@Getter
-//@RequiredArgsConstructor
-//public class HelloResponseDto {
-//
-//    private final String name;
-//    private final int amount;
-//
-//}
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
